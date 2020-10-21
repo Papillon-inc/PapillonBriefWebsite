c153d62753e862de6a92e436117e495dc1f53d0d
Papillon6814 <so1rou.k@gmail.com>
2020-10-21 19:05:56 +0900

    scss導入と画像を保存

---
+++ papillon_brief_website/components/NavigationBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navigationBar">
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.navigationBar {
+  height: 80px;
+  width: 100%;
+
+  background: red;
+}
+</style>
