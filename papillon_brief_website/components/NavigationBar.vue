8fc4ceb95dbb1ec45ca8ace04103652b4bebe690
Papillon6814 <so1rou.k@gmail.com>
2020-10-22 02:41:21 +0900

    メニューのいい感じの配置のしかた

--- papillon_brief_website/components/NavigationBar.vue
+++ papillon_brief_website/components/NavigationBar.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="navigationBar">
+    <div class="papillonPos">
+      <img src="~/static/image/papillon.png" class="papillonImg" />
+    </div>
+    <div class="menu">
+      <span class="product">Product</span>
+      <span class="news">News</span>
+      <span class="contact">Contact</span>
+      <span class="info">Company Info</span>
+    </div>
   </div>
 </template>
 
@@ -7,10 +16,73 @@
 </script>
 
 <style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap');
+
 .navigationBar {
   height: 80px;
   width: 100%;
 
-  background: red;
+  background: white;
+  box-shadow: #dbdbdb 2px 2px 2px;
+
+  .papillonPos {
+    position: relative;
+    left: 20%;
+    top: 10px;
+
+    .papillonImg {
+      height: 60px;
+      width: 60px;
+    }
+  }
+
+  .papillonPos::after {
+    content: "Papillon";
+    position: absolute;
+    top: 16px;
+    margin-left: 5px;
+
+    font-weight: 900;
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    color: #212121;
+  }
+
+  .menu {
+    position: absolute;
+    right: 10%;
+    color: #767676;
+
+    .product {
+      position: relative;
+      top: -36px;
+      right: 30px;
+      font-size: 24px;
+      text-align: right;
+    }
+
+    .news {
+      position: relative;
+      top: -36px;
+      right: 20px;
+      font-size: 24px;
+      text-align: right;
+    }
+
+    .contact {
+      position: relative;
+      right: 10px;
+      top: -36px;
+      font-size: 24px;
+      text-align: right;
+    }
+
+    .info {
+      position: relative;
+      top: -36px;
+      font-size: 24px;
+      text-align: right;
+    }
+  }
 }
 </style>
--- papillon_brief_website/pages/index.vue
+++ papillon_brief_website/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
- <Navigation />
+  <div class="container">
+    <Navigation class="navi"/>
+    <div class="firstPanel">
+      
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,6 +17,30 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+.container {
+  margin: 0;
+  height: 2400px;
 
+  .navi {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    //FIXME: 影の付け方が雑
+    z-index: 2;
+  }
+
+  .firstPanel {
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 3;
+
+    height: 1060px;
+    width: 100%;
+
+    background: #fafafa;
+  }
+}
 </style>
