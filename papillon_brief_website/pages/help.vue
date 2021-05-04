643592cc761285f2ed04fdf90ddd280dd7d15c41
Papillon6814 <so1rou.k@gmail.com>
2021-05-05 01:55:20 +0900

    feat: ヘルプ更新

--- papillon_brief_website/pages/help.vue
+++ papillon_brief_website/pages/help.vue
@@ -2,7 +2,10 @@
 	<div class="container">
 		ヘルプのページ<br>
 		<br>
-		<a href="https://discord.gg/cfZw6EAYrv">https://discord.gg/cfZw6EAYrv</a>
+		コミュニティサーバー<br>
+		<a href="https://discord.gg/cfZw6EAYrv">https://discord.gg/cfZw6EAYrv</a><br>
+		メールアドレス<br>
+		<a href="mailto:info@papillon.co.jp">info@papillon.co.jp</a><br>
 	</div>
 </template>
 
