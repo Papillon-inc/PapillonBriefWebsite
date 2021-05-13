b9b17e904fef8647e9faab378e5d45b14aab175e
Papillon6814 <so1rou.k@gmail.com>
2021-05-13 17:33:34 +0900

    feat: 検索まで

---
+++ papillon_brief_website/components/SearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="searchbar-container">
+        <font-awesome-icon icon="search" class="search" />
+        <input type="text" autocomplete="on" list="suggestion" placeholder="質問をしてみてください...">
+        <datalist id="suggestion">
+            <option value="候補A"></option>
+            <option value="候補B"></option>
+            <option value="候補C"></option>
+        </datalist>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap');
+
+.searchbar-container {
+    width: 100%;
+    height: 100px;
+
+    input {
+        padding-left: 10%;
+        width: 100%;
+        height: 100px;
+        font-size: 26px;
+        border-radius: 100px;
+        font-family: 'Noto Sans JP', sans-serif;
+        border: none;
+        box-shadow: 5px 5px 20px #00000029;
+    }
+
+    .search {
+        display: block;
+        position: absolute;
+        margin-top: 36px;
+        margin-left: 5%;
+        width: 36px;
+        height: 36px;
+        color: #767676;
+    }
+}
+</style>
--- papillon_brief_website/pages/help/index.vue
+++ papillon_brief_website/pages/help/index.vue
@@ -9,31 +9,41 @@
             <div class="subtitle">
                 何か手伝えることはありませんか？
             </div>
+            <search-bar class="search-bar" />
         </div>
+        <Footer />
     </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import SearchBar from '~/components/SearchBar.vue'
+import Footer from '~/components/Footer.vue'
 
 export default {
     components: {
-        Header
+        Header,
+        SearchBar,
+        Footer
     }
 }
 </script>
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap');
 
 .container {
+    background: #fafafa;
+
     .title-block {
         position: relative;
         margin-top: 260px;
         margin-left: 10%;
+        margin-bottom: 200px;
 
         width: 80%;
-        background: cyan;
+        //background: cyan;
 
         .title {
             font-size: 72px;
@@ -45,6 +55,11 @@ export default {
             font-size: 32px;
             color: #767676;
             font-weight: 500;
+            font-family: 'Noto Sans JP', sans-serif;
+        }
+
+        .search-bar {
+            margin-top: 55px;
         }
     }
 }
