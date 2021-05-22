8158477d16478d8ccb9d2979011dbca87099eb06
Papillon6814 <so1rou.k@gmail.com>
2021-05-23 02:41:21 +0900

    feat: participant help

---
+++ papillon_brief_website/pages/help/participant/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <Header />
+        <div class="content">
+            <h1>参加者スタートガイド</h1>
+            ここではどのように大会に参加するのかについて説明します。
+            <h2>大会を見つける</h2>
+            <img src="~/static/image/help/participant/home.png" /><br>
+            大会は基本的にHOME画面から見つけることができます。<br>
+            自分が興味のあるゲーム、大会名から検索をして大会を絞り込むこともできます。<br>
+            <img src="~/static/image/help/participant/info1.png" />
+            <img src="~/static/image/help/participant/info2.png" /><br>
+            興味のある大会を選択すると、大会の情報が表示されます。<br>
+            「参加」ボタンをタップすることで大会に参加登録することができます。<br>
+            <img src="~/static/image/help/participant/leave.png" /><br>
+            参加は「抜ける」ボタンで取り消すことができます。<br>
+            なお、大会の開催時刻が被ってしまう場合は参加登録することができないようにしています。<br>
+            <img src="~/static/image/help/participant/enter.png" /><br>
+            参加登録をした大会はエントリーしている大会一覧の画面（以下、エントリー画面）に表示されます。<br>
+            
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+    background: #fafafa;
+
+    .content {
+        position: relative;
+        margin-top: 200px;
+
+        img {
+            width: 260px;
+        }
+    }
+}
+</style>
